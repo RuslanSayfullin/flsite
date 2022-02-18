e5dea57dd61e18cb23b77c5ef69ad1471c4d350b
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-02-18 11:16:12 +0500

    Поделиться статьей через e-mail

--- clipper/blog/views.py
+++ clipper/blog/views.py
@@ -1,4 +1,5 @@
 from django.core import paginator
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
@@ -17,16 +18,24 @@ class PostListView(ListView):
 def post_share(request, post_id):
     # Получение статьи по идентификатору.
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
     if request.method == 'POST':
         # Форма была отправлена на сохранение.
         form = EmailPostForm(request.POST)
         if form.is_valid():     # Все поля формы прошли валидацию.
             cd = form.cleaned_data
-            # ... Отправка электронной почты.
+            # Отправка электронной почты.
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "' \
+                      '{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments:{}'\
+                .format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
         else:
             form = EmailPostForm()
             return render(request, 'blog/post/share.html',
-                          {'post': post, 'form': form})
+                          {'post': post, 'form': form, 'sent': sent})
 
 
 # def post_list(request):
