4f476d7f2a3f56e583252116527744d6f490164b
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-02-18 15:38:04 +0500

    Добавили полнотекстовый поиск с помощью PostgreSQL

--- clipper/blog/views.py
+++ clipper/blog/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.db.models import Count
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 
 
@@ -83,3 +84,17 @@ def post_detail(request, year, month, day, post):
     return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment,
                                                      'comment_form': comment_form, 'similar_posts': similar_posts})
 
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+    if form.is_valid():
+        query = form.cleaned_data['query']
+        search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+        search_query = SearchQuery(query)
+        results = Post.objects.annotate(search=search_vector, rank=SearchRank(search_vector, search_query))\
+            .filter(rank__gte=0.3).order_by('-rank')
+        return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
