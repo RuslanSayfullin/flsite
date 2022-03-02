d969bfd23c54be6cf6405a2998288c5ee51ebfe5
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-03-02 16:23:26 +0500

    Использование сигналов D jango

--- clipper/images/apps.py
+++ clipper/images/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ImagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'images'
+
+    def ready(self):
+        # Импортируем файл с описанной функцией-подписчиком на сигнал.
+        import images.signals
