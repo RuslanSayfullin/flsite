601e374d99fffe3f1316bc2818a9dd7d3dc8a84b
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-03-02 15:33:04 +0500

    Добавление активности в новостную ленту

--- clipper/account/views.py
+++ clipper/account/views.py
@@ -8,6 +8,7 @@ from .models import Profile, Contact
 from django.contrib import messages
 from django.views.decorators.http import require_POST
 from clipper.common.decorators import ajax_required
+from actions.utils import create_action
 
 
 def user_login(request):
@@ -44,6 +45,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             # Создание профиля пользователя.
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             # Сохраняем пользователя в базе данных.
             new_user.save()
             return render(request, 'account/register_done.html', {'new_user': new_user})
@@ -92,6 +94,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
             return JsonResponse({'status': 'ok'})
