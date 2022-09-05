e8ae15fcc2048a946ea867583289993afdb9c45c
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-09-05 10:22:51 +0500

    Создали, основное приложение.

--- clipper/clipper/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-from celery import Celery
-
-# Задаем переменную окружения, содержащую название файла настроек нашего проекта.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
-
-app = Celery('clipper')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
--- clipper/images/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'images'
-urlpatterns = [
-    path('create/', views.image_create, name='create'),
-    path('like/', views.image_like, name='like'),
-    path('', views.image_list, name='list'),
-    path('ranking/', views.image_ranking, name='create'),
-]
