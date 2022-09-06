86a29f5e1e4e3787ab25e27a5fca3898f6b36760
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-09-06 09:43:45 +0500

    Добавили параметры в обработчики, для шаблонов.

--- flsite.py
+++ flsite.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="Про Flask.")
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title="О сайте.")
 
 
 if __name__ == "__main__":
