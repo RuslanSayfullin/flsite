7fe2012a78978a2622a039919b042fb35849ba04
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-09-07 10:23:24 +0500

    Добавили динамические url-адреса.

--- flsite.py
+++ flsite.py
@@ -7,19 +7,22 @@ menu = ["Установка", "Первое приложение", "Обратн
 
 @app.route("/")
 def index():
-    print(url_for('index'))
     return render_template('index.html', title="Про Flask.", menu=menu)
 
 
 @app.route("/about")
 def about():
-    print(url_for('about'))
     return render_template('about.html', title="О сайте.", menu=menu)
 
 
-with app.test_request_context():
-    print(url_for('index'))
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Пользователь: {username}"
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+# with app.test_request_context():
+#     print(url_for('index'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
