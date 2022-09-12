3e0d33b76b12d10ac445b7a02d1895908807ada9
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-09-12 10:26:00 +0500

    ПОдключили БД и сформировали таблицы.

--- flsite.py
+++ flsite.py
@@ -13,54 +13,70 @@ app.config.from_object(__name__)
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
 
-menu = [{"name": "Установка", "url": "install-flask"},
-        {"name": "Первое приложение", "url": "first-app"},
-        {"name": "Обратная связь", "url": "contact"}]
+def connect_db():
+    """Подключаем БД."""
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
-@app.route("/")
-def index():
-    return render_template('index.html', title="Про Flask.", menu=menu)
+def create_db():
+    """Вспомогательная функция для создания таблиц БД, без запуска веб-сервера."""
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
 
 
-@app.route("/about")
-def about():
-    return render_template('about.html', title="О сайте.", menu=menu)
-
-
-@app.route("/contact", methods=["POST", "GET"])
-def contact():
-    if request.method == 'POST':
-        if len(request.form['username']) > 2:
-            flash('Сообщение отправлено', category='success')
-        else:
-            flash('Ошибка отправки', category='error')
-
-    return render_template('contact.html', title="Обратная связь.", menu=menu)
-
-
-@app.route("/profile/<username>")
-def profile(username):
-    if 'userLogged' not in session or session['userLogged'] != username:
-        abort(401)
-
-    return f"Пользователь: {username}"
-
-
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if 'userLogged' in session:
-        return redirect(url_for('profile', username=session['userLogged']))
-    elif request.method == 'POST' and request.form['username'] == "cryptolis" and request.form['psw'] == "123":
-        session['userLogged'] = request.form['username']
-        return redirect(url_for('profile', username=session['userLogged']))
-
-    return render_template("login.html", title="Авторизация", menu=menu)
-
-
-@app.errorhandler(404)
-def pageNotFound(error):
-    return render_template('page404.html', title="Страница не найдена", menu=menu), 404
+# menu = [{"name": "Установка", "url": "install-flask"},
+#         {"name": "Первое приложение", "url": "first-app"},
+#         {"name": "Обратная связь", "url": "contact"}]
+#
+#
+# @app.route("/")
+# def index():
+#     return render_template('index.html', title="Про Flask.", menu=menu)
+#
+#
+# @app.route("/about")
+# def about():
+#     return render_template('about.html', title="О сайте.", menu=menu)
+#
+#
+# @app.route("/contact", methods=["POST", "GET"])
+# def contact():
+#     if request.method == 'POST':
+#         if len(request.form['username']) > 2:
+#             flash('Сообщение отправлено', category='success')
+#         else:
+#             flash('Ошибка отправки', category='error')
+#
+#     return render_template('contact.html', title="Обратная связь.", menu=menu)
+#
+#
+# @app.route("/profile/<username>")
+# def profile(username):
+#     if 'userLogged' not in session or session['userLogged'] != username:
+#         abort(401)
+#
+#     return f"Пользователь: {username}"
+#
+#
+# @app.route("/login", methods=["POST", "GET"])
+# def login():
+#     if 'userLogged' in session:
+#         return redirect(url_for('profile', username=session['userLogged']))
+#     elif request.method == 'POST' and request.form['username'] == "cryptolis" and request.form['psw'] == "123":
+#         session['userLogged'] = request.form['username']
+#         return redirect(url_for('profile', username=session['userLogged']))
+#
+#     return render_template("login.html", title="Авторизация", menu=menu)
+#
+#
+# @app.errorhandler(404)
+# def pageNotFound(error):
+#     return render_template('page404.html', title="Страница не найдена", menu=menu), 404
 
 
 if __name__ == "__main__":
