9cdf7488a3054eca01b1e69692b303e58986f809
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-09-13 10:24:01 +0500

    Подключили возможность загружать меню изи БД.

--- flsite.py
+++ flsite.py
@@ -2,6 +2,8 @@ import sqlite3
 import os
 from flask import Flask, render_template, g, url_for, request, flash, session, redirect, abort
 
+from FDataBase import FDataBase
+
 # конфигурация
 DATABASE = '/tmp/flsite.db'
 DEBUG = True
@@ -39,7 +41,8 @@ def get_db():
 @app.route("/")
 def index():
     db = get_db
-    return render_template('index.html', title="Про Flask.", menu=[])
+    dbase = FDataBase(db)
+    return render_template('index.html', title="Про Flask.", menu=dbase.getMenu())
 
 
 @app.teardown_appcontext
@@ -47,56 +50,6 @@ def close_db(error):
     """Закрываем соединение с БД, если оно было установлено"""
     if hasattr(g, 'link_db'):
         g.link_db.close()
-
-
-# menu = [{"name": "Установка", "url": "install-flask"},
-#         {"name": "Первое приложение", "url": "first-app"},
-#         {"name": "Обратная связь", "url": "contact"}]
-#
-#
-# @app.route("/")
-# def index():
-#     return render_template('index.html', title="Про Flask.", menu=menu)
-#
-#
-# @app.route("/about")
-# def about():
-#     return render_template('about.html', title="О сайте.", menu=menu)
-#
-#
-# @app.route("/contact", methods=["POST", "GET"])
-# def contact():
-#     if request.method == 'POST':
-#         if len(request.form['username']) > 2:
-#             flash('Сообщение отправлено', category='success')
-#         else:
-#             flash('Ошибка отправки', category='error')
-#
-#     return render_template('contact.html', title="Обратная связь.", menu=menu)
-#
-#
-# @app.route("/profile/<username>")
-# def profile(username):
-#     if 'userLogged' not in session or session['userLogged'] != username:
-#         abort(401)
-#
-#     return f"Пользователь: {username}"
-#
-#
-# @app.route("/login", methods=["POST", "GET"])
-# def login():
-#     if 'userLogged' in session:
-#         return redirect(url_for('profile', username=session['userLogged']))
-#     elif request.method == 'POST' and request.form['username'] == "cryptolis" and request.form['psw'] == "123":
-#         session['userLogged'] = request.form['username']
-#         return redirect(url_for('profile', username=session['userLogged']))
-#
-#     return render_template("login.html", title="Авторизация", menu=menu)
-#
-#
-# @app.errorhandler(404)
-# def pageNotFound(error):
-#     return render_template('page404.html', title="Страница не найдена", menu=menu), 404
 
 
 if __name__ == "__main__":
