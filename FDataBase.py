07d3a419ea465ee91644c68f989c187116c64356
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-09-14 10:37:20 +0500

    Переместили,  регулярное выражение.

--- FDataBase.py
+++ FDataBase.py
@@ -29,6 +29,11 @@ class FDataBase:
                 print("Статья с таким url уже существует")
                 return False
 
+            base = url_for('static', filename='images_html')
+            text = re.sub(r"(?P<tag><img\s+[^>]*src=)(?P<quote>[\"'])(?P<url>.+?)(?P=quote)>",
+                          "\\g<tag>" + base + "/\\g<url>>",
+                          text)
+
             tm = math.floor(time.time())
             self.__cur.execute("INSERT INTO posts VALUES(NULL, ?, ?, ?, ?)", (title, text, url, tm))
             self.__db.commit()
@@ -43,12 +48,7 @@ class FDataBase:
             self.__cur.execute(f"SELECT title, text FROM posts WHERE url LIKE {alias} LIMIT 1")
             res = self.__cur.fetchone()
             if res:
-                base = url_for('static', filename='images_html')
-                text = re.sub(r"(?P<tag><img\s+[^>]*src=)(?P<quote>[\"'])(?P<url>.+?)(?P=quote)>",
-                              "\\g<tag>" + base + "/\\g<url>>",
-                              res['text'])
-
-                return (res['title'], text)
+                return res
         except sqlite3.Error as e:
             print("Ошибка получения статьи из БД " + str(e))
 
