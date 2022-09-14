26104ca87e61c7b28eb255c381e662b0793943b4
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-09-14 10:32:58 +0500

    Прописали регулярное выражение, для получения статей по url

--- FDataBase.py
+++ FDataBase.py
@@ -1,6 +1,8 @@
+import re
 import sqlite3
 import time
 import math
+from flask import url_for
 
 
 class FDataBase:
@@ -41,7 +43,12 @@ class FDataBase:
             self.__cur.execute(f"SELECT title, text FROM posts WHERE url LIKE {alias} LIMIT 1")
             res = self.__cur.fetchone()
             if res:
-                return res
+                base = url_for('static', filename='images_html')
+                text = re.sub(r"(?P<tag><img\s+[^>]*src=)(?P<quote>[\"'])(?P<url>.+?)(?P=quote)>",
+                              "\\g<tag>" + base + "/\\g<url>>",
+                              res['text'])
+
+                return (res['title'], text)
         except sqlite3.Error as e:
             print("Ошибка получения статьи из БД " + str(e))
 
