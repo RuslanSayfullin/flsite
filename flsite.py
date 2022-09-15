6fc12d500246942ad01704f6be11dd1d6d3e5328
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-09-15 10:31:33 +0500

    Добавлено хэширование паролей в БД.

--- flsite.py
+++ flsite.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, render_template, g, url_for, request, flash, session, redirect, abort
 
 from FDataBase import FDataBase
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # конфигурация
 DATABASE = '/tmp/flsite.db'
@@ -86,8 +87,21 @@ def login():
     return render_template("login.html", menu=dbase.getMenu(), title="Авторизация")
 
 
-@app.route("/register")
+@app.route("/register", methods=["POST", "GET"])
 def register():
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['email']) > 4 and len(request.form['psw']) > 4 and\
+                request.form['psw'] == request.form['psw2']:
+            hash = generate_password_hash(request.form['psw'])
+            res = dbase.addUser(request.form['name'], request.form['email'], hash)
+            if res:
+                flash("Вы успешно зарегистрированы", "success")
+                return redirect(url_for('login'))
+            else:
+                flash("Ошибка при добавлении в БД", "error")
+        else:
+            flash("Неверно заполнены поля", "error")
+
     return render_template("register.html", menu=dbase.getMenu(), title="Регистрация")
 
 
