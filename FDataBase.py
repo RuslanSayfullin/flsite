f42bad3c3473a3ec22af720c0d72c94a51260e65
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-09-16 10:27:10 +0500

    Добавили функционал для извлечения пользователя из  БД, при авторизаций.

--- FDataBase.py
+++ FDataBase.py
@@ -82,6 +82,20 @@ class FDataBase:
 
         return True
 
+    def getUser(self, user_id):
+        try:
+            self.__cur.execute(f"SELECT * FROM users WHERE id = {user_id} LIMIT 1")
+            res = self.__cur.fetchone()
+            if not res:
+                print("Пользователь не найден")
+                return False
+
+            return res
+        except sqlite3.Error as e:
+            print("Ошибка получения данных из БД " + str(e))
+
+        return False
+
 
 
 
