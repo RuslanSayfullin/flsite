3c5cf97f9b9b000ac67e769c1023b8f5dcc95313
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-09-16 10:32:21 +0500

    Добавили функционал для извлечения пользователя из  БД по e-mail, при авторизаций.

--- FDataBase.py
+++ FDataBase.py
@@ -96,6 +96,20 @@ class FDataBase:
 
         return False
 
+    def getUserByEmail(self, email):
+        try:
+            self.__cur.execute(f"SELECT * FROM users WHERE email = '{email}' LIMIT 1")
+            res = self.__cur.fetchone()
+            if not res:
+                print("Пользователь не найден")
+                return False
+
+            return res
+        except sqlite3.Error as e:
+            print("Ошибка получения данных из БД " + str(e))
+
+        return False
+
 
 
 
