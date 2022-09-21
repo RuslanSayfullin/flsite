d4a11816a5da68c3e3124d7e22e55d30f31f060d
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-09-21 10:53:51 +0500

    Добавили вспомогательные функции и обработчик выхода из профиля в модуле админ-панели.

--- admin/admin.py
+++ admin/admin.py
@@ -3,8 +3,16 @@ from flask import Blueprint, render_template, url_for, redirect, session, reques
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
 
 
+def isLogged():
+    return True if session.get('admin_logged') else False
+
+
 def login_admin():
     session['admin_logged'] = 1
+
+
+def logout_admin():
+    session.pop('admin_logged', None)
 
 
 @admin.route('/')
@@ -14,6 +22,7 @@ def index():
 
 @admin.route('/login', methods=["POST", "GET"])
 def login():
+
     if request.method == "POST":
         if request.form['user'] == "admin" and request.form['psw'] == "12345":
             login_admin()
@@ -22,4 +31,14 @@ def login():
             flash("Неверная пара логин/пароль", "error")
 
     return render_template('admin/login.html', title='Админ-панель')
+
+
+@admin.route('/logout', methods=["POST", "GET"])
+def logout():
+    if not isLogged():
+        return redirect(url_for('.login'))
+
+    logout_admin()
+
+    return redirect(url_for('.login'))
 
