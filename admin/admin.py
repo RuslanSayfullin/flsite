72b4a093c0d4981b25bb87bd7adaaa0b458d2421
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-09-21 11:14:48 +0500

    Настроили подключение к БД в админ-панели.

--- admin/admin.py
+++ admin/admin.py
@@ -2,6 +2,11 @@ from flask import Blueprint, render_template, url_for, redirect, session, reques
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
 
+menu = [{'url': '.index', 'title': 'Панель'},
+        {'url': '.listusers', 'title': 'Список пользователей'},
+        {'url': '.listpubs', 'title': 'Список статей'},
+        {'url': '.logout', 'title': 'Выйти'}]
+
 
 def isLogged():
     return True if session.get('admin_logged') else False
@@ -15,13 +20,33 @@ def logout_admin():
     session.pop('admin_logged', None)
 
 
+db = None   # Инициализируем подключение к БД
+@admin.before_request
+def before_request():
+    """Установление соединения с БД перед выполнением запроса"""
+    global db
+    db = g.get('link_db')
+
+
+@admin.teardown_request
+def teardown_request(request):
+    global db
+    db = None
+    return request
+
+
 @admin.route('/')
 def index():
-    return "admin"
+    if not isLogged():
+        return redirect(url_for('.login'))
+
+    return render_template('admin/index.html', menu=menu, title='Админ-панель')
 
 
 @admin.route('/login', methods=["POST", "GET"])
 def login():
+    if isLogged():
+        return redirect(url_for('.index'))
 
     if request.method == "POST":
         if request.form['user'] == "admin" and request.form['psw'] == "12345":
