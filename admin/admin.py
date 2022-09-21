30ff99b7f1e33e41adf126464b1f093d6a00998a
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-09-21 10:48:53 +0500

    Добавили обработчики главой страницы и авторизаций, для модуля админ-панели.

--- admin/admin.py
+++ admin/admin.py
@@ -1,3 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, url_for, redirect, session, request, flash, g
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
+
+
+def login_admin():
+    session['admin_logged'] = 1
+
+
+@admin.route('/')
+def index():
+    return "admin"
+
+
+@admin.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        if request.form['user'] == "admin" and request.form['psw'] == "12345":
+            login_admin()
+            return redirect(url_for('.index'))
+        else:
+            flash("Неверная пара логин/пароль", "error")
+
+    return render_template('admin/login.html', title='Админ-панель')
+
