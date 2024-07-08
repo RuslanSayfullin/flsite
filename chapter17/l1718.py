43efeec8351446355c7e4626734d3c344d8d1d4c
Sayfullin Ruslan <ruslansaifullin91@gmail.com>
2024-07-08 10:12:53 +0500

    Начал заново

--- chapter17/l1718.py
+++
@@ -1,9 +0,0 @@
-def change_x(self):
-    ml = QtCore.QMutexLocker(MyThread.mutex)
-    print("x =", MyThread.x, "id =", self.id)
-    MyThread.x += 5
-    self.sleep(2)
-    print("x =", MyThread.x, "id =", self.id)
-    MyThread.x += 34
-    print("x =", MyThread.x, "id =", self.id)
-    # Блокировка автоматически снимется
--- old/l205loadUi.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from PyQt5 import QtWidgets, uic
-import sys
-app = QtWidgets.QApplication(sys.argv)
-window = uic.loadUi("MyForm.ui")
-window.btnQuit.clicked.connect(app.quit)
-window.show()
-sys.exit(app.exec_())
