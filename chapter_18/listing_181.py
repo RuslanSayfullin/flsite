b5dc647855fe5359ee02c43aa544c4032019d7a2
Sayfullin Ruslan <ruslansaifullin91@gmail.com>
2024-07-08 16:31:08 +0500

    Первая программа

---
+++ chapter_18/listing_181.py
@@ -0,0 +1,16 @@
+from PyQt6 import QtWidgets
+import sys
+
+app = QtWidgets.QApplication(sys.argv)
+window = QtWidgets.QWidget()
+window.setWindowTitle("Пepвaя программа на PyQt")
+window.resize(300, 70)
+label = QtWidgets.QLabel("<center>Пpивeт, миp!</center>")
+ЬtnQuit = QtWidgets.QPushButton("&Закрыть окно")
+vbox = QtWidgets.QVBoxLayout()
+vbox.addWidget(label)
+vbox.addWidget(ЬtnQuit)
+window.setLayout(vbox)
+ЬtnQuit.clicked.connect(app.quit)
+window.show()
+sys.exit(app.exec())
